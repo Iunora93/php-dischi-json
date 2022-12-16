ad335e6ee5de7d470bbc827e4e0426b320946bc4
Iunora93 <eleonoraruggieri22@gmail.com>
2022-12-16 13:55:13 +0100

    first commit

---
+++ api.php
@@ -0,0 +1,7 @@
+<?php
+$dischi = file_get_contents("dischi.json");
+
+$dischi = json_decode($dischi, true);
+$json_string = json_encode($dischi);
+header("Content-Type: application/json");
+echo $json_string;
---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+      return {
+        message: 'Hello Vue!',
+      };
+    },
+    created(){
+        axios.get("http://localhost/php-dischi-json/api.php").then((res) => {
+        console.log(res.data);
+        });
+    },
+}).mount('#app');
